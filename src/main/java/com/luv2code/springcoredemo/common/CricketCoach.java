553fb4fd3e6d5a3ea94f236f09d8a994c5d7e9ef
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-27 23:13:28 +0100

    bean @PostConstruct and @PreDestroy method annotations

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,16 +1,30 @@
 package com.luv2code.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // new instance each injection
+// @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // new instance each injection
 public class CricketCoach implements Coach {
 
   public CricketCoach() {
     System.out.println("In constructor: " + getClass().getSimpleName());
+  }
+
+  // define our init method
+  @PostConstruct
+  public void doMyStartupStuff() {
+    System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
+  }
+
+  // define our destroy method
+  @PreDestroy
+  public void doMyCleanupStuff() {
+    System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
   }
 
   @Override
