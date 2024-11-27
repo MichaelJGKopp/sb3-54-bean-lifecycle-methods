6db502ac950fd7ab5f75d830bd1cfe9d261a2205
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-27 19:01:15 +0100

    println diagnostics

--- src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class TrackCoach implements Coach {
 
+  public TrackCoach() {
+    System.out.println("In constructor: " + getClass().getSimpleName());
+  }
+
   @Override
   public String getDailyWorkout() {
     return "TrackCoach here, practice running";
