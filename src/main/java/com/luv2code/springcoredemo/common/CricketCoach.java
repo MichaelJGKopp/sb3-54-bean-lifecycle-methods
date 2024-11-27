3f33968630fc26743861cba53b8ea6e6d68292b6
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-27 22:38:38 +0100

    use prototype scope, new instance each injection

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,12 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // new instance each injection
 public class CricketCoach implements Coach {
 
   public CricketCoach() {
